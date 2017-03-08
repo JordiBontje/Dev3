ecea8838dc35f197089605b078c81a37cad1921d
MischienWelJordi <jordibontje11@gmail.com>
2017-03-08 15:03:05 +0100

    Add project files.

---
+++ Homework 1.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1._2
+{
+    class UserStory
+    {
+        private int hours { get; set;}
+        private string description { get; set;}
+
+        public UserStory(int hours, string description)
+        {
+            this.hours = hours;
+            this.description = description;
+        }
+
+        public string ToString()
+        {
+            return "UserStory:" + this.description + "will take" + this.hours;
+        }
+
+
+        static void Main(string[] args)
+        {
+            UserStory U1 = new UserStory(5, "lekker leuk");
+            UserStory U2 = new UserStory(7, "HEHE EKSDIE");
+            UserStory U3 = new UserStory(9, "Fijn");
+        }
+    }
+}
---
+++ Homework 1.2/Sprint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Homework_1._2
+{
+    class Sprint
+    {
+        string[] UserStories = new string[] {UserStory.U1, };
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
